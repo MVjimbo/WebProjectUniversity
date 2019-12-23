b63f67cba2d08d415326da5f505881405bd296fd
MVjimbo <maslo-86@yandex.ru>
2019-12-23 14:59:04 +0300

    small change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,8 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{url('/')}}">Home </a>
                         </li>
+                        <li><a class="nav-link" href="#">{{ route('login') }}</a>
+                        </li>
                         @if(\Auth::check())
                         <li class="nav-item">
                             <a class="nav-link" href="{{url('/post/create')}}">Add Post</a>
